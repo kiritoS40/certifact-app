fa6b7d45a670fc2654145cb3641c9c3c4d5b2ae0
Mark David Prado <155267500+kiritoS40@users.noreply.github.com>
2024-01-11 21:04:04 +0800

    Add NewFactForm, CategoryFilter, and FactList components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import "./style.css";
 
 function App() {
-  return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" height="68" width="68" alt="Certifact Logo" />
-        <h1>Certifact</h1>
-      </div>
+  const appTitle = "Certifact";
 
-      <button className="btn btn-large btn-open">Share a fact</button>
-    </header>
+  return (
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" height="68" width="68" alt="Certifact Logo" />
+          <h1>{appTitle}</h1>
+        </div>
+
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+
+      <NewFactForm />
+      <main class="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact form</form>;
+}
+
+function CategoryFilter() {
+  return <aside>Category Filter</aside>;
+}
+
+function FactList() {
+  return <section>Facts List</section>;
 }
 
 export default App;
