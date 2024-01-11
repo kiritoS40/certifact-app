8611e8cd6895e270b6d9125d5ce0550325251833
Mark David Prado <155267500+kiritoS40@users.noreply.github.com>
2024-01-11 20:41:52 +0800

    Integrate header component into the React app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import "./style.css";
+
 function App() {
-  return <h1>Certifact</h1>;
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" height="68" width="68" alt="Certifact Logo" />
+        <h1>Certifact</h1>
+      </div>
+
+      <button className="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
 }
 
 export default App;
