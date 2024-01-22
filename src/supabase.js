f6d6f27f854b029d84d99c1e2501be9119be3c98
Mark David Prado <155267500+kiritoS40@users.noreply.github.com>
2024-01-22 23:20:46 +0800

    Export supabase

--- src/supabase.js
+++ src/supabase.js
@@ -4,3 +4,5 @@ const supabaseUrl = "https://wcfkenmpydolwuzgjpra.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjZmtlbm1weWRvbHd1emdqcHJhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ5NTc1MzgsImV4cCI6MjAyMDUzMzUzOH0.BhHQY3HlMW3SpKQJt-kT-A2TbCVN8YMfKf8bZb6NlCg";
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
